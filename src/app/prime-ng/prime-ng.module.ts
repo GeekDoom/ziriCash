bc3798d732983c145cfa47467c0951351941feee
Jose Goyos <josegoyos1@gmail.com>
2022-03-30 16:15:51 -0500

    minor changes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 import { ImageModule } from 'primeng/image';
 import { MenubarModule } from 'primeng/menubar';
 import { RippleModule } from 'primeng/ripple';
@@ -8,10 +9,12 @@ import { RippleModule } from 'primeng/ripple';
 @NgModule({
   exports: [
     ButtonModule,
+    DialogModule,
     ImageModule,
     MenubarModule,
-    RippleModule
-    
+    RippleModule,
+
+
   ]
 })
 export class PrimeNgModule { }
