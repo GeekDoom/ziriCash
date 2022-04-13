00217b664aafddd21a3f5c382f078d64d260eacc
Jose Goyos <josegoyos1@gmail.com>
2022-04-13 13:31:43 -0500

    programmatic control

--- src/app/modules/user/wallet-movements/wallet-movements.component.ts
+++ src/app/modules/user/wallet-movements/wallet-movements.component.ts
@@ -7,11 +7,24 @@ import { TablesService } from '../../../shared/services/tables.service';
   selector: 'app-wallet-movements',
   templateUrl: './wallet-movements.component.html',
   styles: [
+    `:host ::ng-deep .p-datatable .p-datatable-thead > tr > th {
+            position: -webkit-sticky;
+            position: sticky;
+            top: 0px;
+        }
+        @media screen and (max-width: 64em) {
+            :host ::ng-deep .p-datatable .p-datatable-thead > tr > th {
+                top: 0px;
+            }
+        }
+      `
   ]
 })
 export class WalletMovementsComponent implements OnInit {
-  movements: Movements[] = [];
+  movements?: Movements[]
+  first = 0;
 
+  rows = 10;
   constructor(private ts: TablesService) { }
 
   ngOnInit(): void {
@@ -19,6 +32,26 @@ export class WalletMovementsComponent implements OnInit {
       .subscribe(mov => {
         this.movements = mov
       })
+  }
+
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.movements ? this.first === (this.movements.length - this.rows) : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.movements ? this.first === 0 : true;
   }
 
 }
