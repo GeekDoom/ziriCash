b2b3ed509592d548b21b18b3b80f313370121bca
Jose Goyos <josegoyos1@gmail.com>
2022-04-17 11:31:49 -0500

    sort tables exhange and movements

--- src/app/modules/user/exchange/exchange.component.ts
+++ src/app/modules/user/exchange/exchange.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SortEvent } from 'primeng/api';
 import { TablesService } from 'src/app/shared/services/tables.service';
 import { typeOc } from '../../interfaces/selects';
 import { Dashboard } from '../../interfaces/tables';
@@ -55,5 +56,26 @@ export class ExchangeComponent implements OnInit {
 
   isFirstPage(): boolean {
     return this.dashboard ? this.first === 0 : true;
+  }
+
+  customSort(event: SortEvent) {
+    event.data!.sort((data1, data2) => {
+      let value1 = data1[event.field!];
+      let value2 = data2[event.field!];
+      let result = null;
+
+      if (value1 == null && value2 != null)
+        result = -1;
+      else if (value1 != null && value2 == null)
+        result = 1;
+      else if (value1 == null && value2 == null)
+        result = 0;
+      else if (typeof value1 === 'string' && typeof value2 === 'string')
+        result = value1.localeCompare(value2);
+      else
+        result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+
+      return (event.order! * result);
+    });
   }
 }
--- src/app/modules/user/wallet-movements/wallet-movements.component.ts
+++ src/app/modules/user/wallet-movements/wallet-movements.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movements } from '../../interfaces/tables';
 import { TablesService } from '../../../shared/services/tables.service';
+import { SortEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-wallet-movements',
@@ -42,6 +43,27 @@ export class WalletMovementsComponent implements OnInit {
 
   isFirstPage(): boolean {
     return this.movements ? this.first === 0 : true;
+  }
+
+  customSort(event: SortEvent) {
+    event.data!.sort((data1, data2) => {
+      let value1 = data1[event.field!];
+      let value2 = data2[event.field!];
+      let result = null;
+
+      if (value1 == null && value2 != null)
+        result = -1;
+      else if (value1 != null && value2 == null)
+        result = 1;
+      else if (value1 == null && value2 == null)
+        result = 0;
+      else if (typeof value1 === 'string' && typeof value2 === 'string')
+        result = value1.localeCompare(value2);
+      else
+        result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+
+      return (event.order! * result);
+    });
   }
 
 }
