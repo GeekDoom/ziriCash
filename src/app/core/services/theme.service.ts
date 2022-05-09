e199decdb66cd38ed843ddc3cc233617daa681f5
Jose Goyos <josegoyos1@gmail.com>
2022-05-09 13:30:30 -0500

    theme

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -4,20 +4,20 @@ import { DOCUMENT } from '@angular/common'
   providedIn: 'root'
 })
 export class ThemeService {
-  
+
   constructor(@Inject(DOCUMENT) private document: Document) {
     let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
     themeLink.href = localStorage.getItem('Theme')!;
 
-   }
-    
-   switchTheme(theme: string) {
-     let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
-     if (themeLink) {
-       themeLink.href = theme + ".css";
-       localStorage.setItem('Theme', theme + '.css');
-       console.log(localStorage)
-     }
-   }
+  }
+
+  switchTheme(theme: string) {
+    let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = theme + ".css";
+      localStorage.setItem('Theme', theme + '.css');
+      console.log(localStorage)
+    }
+  }
 
 }
